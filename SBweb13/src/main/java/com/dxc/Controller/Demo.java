953b7438463eff882693d85799ecd79a87f0e4c3
spavankumar2 <spavankumar2@host.docker.internal>
2021-11-24 10:52:03 +0530

    First Commit

---
+++ SBweb13/src/main/java/com/dxc/Controller/Demo.java
@@ -0,0 +1,12 @@
+package com.dxc.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+public class Demo {
+      @RequestMapping("home")
+      public String view()
+      {
+    	  return "Hello";
+      }
+}
---
+++ SBweb13/src/main/java/com/dxc/SBweb13Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBweb13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBweb13Application.class, args);
+	}
+
+}
